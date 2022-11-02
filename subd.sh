fb2e416e6fc30ed92c45f6ee43792470591fbd15
benjaminbencsik <116736668+benjaminbencsik@users.noreply.github.com>
2022-11-02 11:26:29 -0400

    Update subd.sh

--- subd.sh
+++ subd.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+#declaring variables 
 DOMAIN=$1
 
 
-amass enum -d $DOMAIN -o amass1.txt
-amass enum -config ~/.config/amass/config.ini -d $DOMAIN -o amass.txt
-subdfinder -d $DOMAIN -o subfinder.txt 
+amass enum -d $DOMAIN.com -o amass1.txt
+amass enum -config ~/.config/amass/config.ini -d $DOMAIN.com -o amass.txt
+subfinder -d $DOMAIN -o subfinder.txt 
 assetfinder --subs-only $DOMAIN | tee assetfinder.txt 
 

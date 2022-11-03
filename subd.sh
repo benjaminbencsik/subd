b78d2f7f2f1ca9b9fff2851df93942bdb02c0bf9
benjaminbencsik <116736668+benjaminbencsik@users.noreply.github.com>
2022-11-03 10:36:06 -0400

    Update subd.sh

--- subd.sh
+++ subd.sh
@@ -3,9 +3,12 @@
 #declaring variables 
 DOMAIN=$1
 
+echo "starting subdomain enumoration process this can take a minute" 
+
+
 
 amass enum -d $DOMAIN.com -o amass1.txt
 amass enum -config ~/.config/amass/config.ini -d $DOMAIN.com -o amass.txt
 subfinder -d $DOMAIN -o subfinder.txt 
 assetfinder --subs-only $DOMAIN | tee assetfinder.txt 
-
+cd ~/tools/Sublist3r/python3 sublist3r.py -d $DOMAIN 
